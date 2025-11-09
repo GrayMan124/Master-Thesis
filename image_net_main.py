e8367e5a8cf0652416f53519dc4fbc4e8426c96a
Antoni Czernek <aczern4@gmail.com>
2025-11-09 16:02:55 +0100

    tiny image net

--- image_net_main.py
+++ image_net_main.py
@@ -6,18 +6,10 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 import torch.nn as nn
 import torch.optim as optim
-#imports from the files
 from utils import count_parameters, train_model
-# from utils import test_model, train_model, count_parameters, MyDataset
-# from models.ResNet import *
-# from data_processing import *
-# from models.TopoResNet import *
-# from models.ResNetPIBlock import *
-# from models.ResNetTopo2Dim import *
-# from models.ResNetTopoBlock import *
-# from models.TopoResNet import *
-# from models.TopoResNetPI import * 
-# from models.ResNet import *
+from torchvision.models import resnet50
+from huggingface_hub import login
+
 
 from models.ResNet50 import ResNet_50
 
@@ -28,6 +20,8 @@ from datasets import load_dataset
 torch.autograd.set_detect_anomaly(True)
 torch.manual_seed(args.seed)
 
+if torch.cuda.is_available():
+    torch.backends.cudnn.benchmark = True
 
 transform_train = transforms.Compose([
         transforms.RandomResizedCrop(224),
@@ -42,57 +36,57 @@ val_transforms = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     
-def apply_train_transformation(batch):
-    # print(batch['image'])
-    # images = [transform_train(img.convert('RGB')) for img in batch['image']]
-    images = transform_train(batch['image'].convert('RGB'))
-    batch['pixel_values'] = images 
+def train_transforms_batched(examples):
+    examples['pixel_values'] = [transform_train(img.convert("RGB")) for img in examples['image']]
+    return examples
 
-    return batch
+def val_transforms_batched(examples):
+    examples['pixel_values'] = [val_transforms(img.convert("RGB")) for img in examples['image']]
+    return examples
 
-
-def apply_val_transformation(batch):
-    images = [val_transforms(img.convert('RGB')) for img in batch['image']]
-    batch['pixel_values'] = images 
-
-    return batch
-def custom_collate(batch_list):
-    inputs = torch.stack([item['pixel_values'] for item in batch_list])
-    
-    labels = torch.tensor([item['label'] for item in batch_list])
-    
-    return inputs, labels
+def fast_collate(batch):
+    pixel_values = torch.stack([item['pixel_values'] for item in batch])
+    labels = torch.tensor([item['label'] for item in batch])
+    return pixel_values, labels
 
 if __name__ == '__main__':
     args.model = "ResNet" 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
     
+    with open('../HF_API_code.txt','r') as file:
+        API_CODE = file.read()
+
+    API_CODE = API_CODE.replace('\n','')
+
+    HUGGINGFACE_TOKEN = API_CODE
+    login(token=HUGGINGFACE_TOKEN)
+
 
     all_var = os.environ
 
     hf_cache = os.getenv("HF_HOME")
     print(f"My Hugging Face cache is at: {hf_cache}")
     
+    ds = load_dataset("zh-plus/tiny-imagenet", streaming= False)
+    
+    ds['train'] = ds['train'].with_transform(train_transforms_batched)
+    ds['valid'] = ds['valid'].with_transform(val_transforms_batched)
 
-    ds = load_dataset("ILSVRC/imagenet-1k",streaming = True)
-
-    train_stream = ds['train'].map(apply_train_transformation)
-    val_stream = ds['validation'].map(apply_val_transformation)
-
-    train_stream_shuffled = train_stream.shuffle(buffer_size= 1000, seed = 42)
+    train_dataset = ds['train'].shuffle(seed = 42)
+    val_dataset = ds['valid']
 
 
     batch_size = 32
     epochs = 50
 
-    train_loader = DataLoader(train_stream_shuffled, batch_size = batch_size, collate_fn = custom_collate, num_workers = 4)
+    train_loader = DataLoader(train_dataset, batch_size = batch_size, collate_fn = fast_collate, num_workers = 4, pin_memory = True, shuffle = True)
 
-    val_loader = DataLoader(val_stream, batch_size = batch_size, collate_fn = custom_collate, num_workers = 4)
+    val_loader = DataLoader(val_dataset, batch_size = batch_size, collate_fn = fast_collate, num_workers = 4, pin_memory = True)
 
     data_loaders = {'train':train_loader, 'val':val_loader}
 
-    model = ResNet_50(3,1000)
+    model = ResNet_50(3,200)
     model.to(device)
 
     criterion = nn.CrossEntropyLoss()
