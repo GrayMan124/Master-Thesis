9c7866fe8fb8b628109e54714002befaba5a4747
Gray <aczern4@gmail.com>
2026-06-22 17:37:40 +0200

    Fix: test scripts

--- src/ph_robust/data_processing/processing.py
+++ src/ph_robust/data_processing/processing.py
@@ -162,7 +162,7 @@ def process_test(data_set, data_path, cfg):
     # data_set = data_set['test']
     save_path = Path(data_path)
     try:
-        stat_path = save_path.resolve().parent
+        stat_path = save_path.resolve().parents[0]
         print(f"Looking for stats in {stat_path}")
         pi_mean, pi_std = load_stats(stat_path)
         print(f"Loaded Training stats -- Mean: {pi_mean}, Std: {pi_std}")
